0e6a5becc88a813560b40f1e367468df29552e03
cooperbraun13 <cooperjbraun13@gmail.com>
2026-05-24 19:08:51 -0800

    feat: initialized Inventory class and added the following methods for its use: addFish, getFish, getTotalValue, clear, and isEmpty

---
+++ src/models/Inventory.ts
@@ -0,0 +1,42 @@
+import type { Fish } from "./Fish";
+
+export class Inventory {
+  constructor() {}
+
+  // Add a fish
+  addFish(fish: Fish, inventory: Fish[]): string {
+    inventory.push(fish);
+    return `You caught a ${fish}!`;
+  }
+
+  // List all fish
+  getFish(inventory: Fish[]): string {
+    const openingLine = "Your fish inventory:\n";
+    const list = inventory
+      .map((fish) => `${fish.name} x${fish.quantity}`)
+      .join("\n");
+    return openingLine + list;
+  }
+
+  // Calculate total value
+  getTotalValue(inventory: Fish[]): number {
+    let total = 0;
+    for (const fish of inventory) {
+      total += fish.value * fish.quantity;
+    }
+    return total;
+  }
+
+  // Clear inventory
+  clear(inventory: Fish[]): void {
+    inventory.length = 0;
+  }
+
+  // Check if inventory is empty
+  isEmpty(inventory: Fish[]): boolean {
+    if (inventory.length === 0) {
+      return true;
+    }
+    return false;
+  }
+}
