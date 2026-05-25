7532aed09000b88c6b623a0883ab6f082737e114
cooperbraun13 <cooperjbraun13@gmail.com>
2026-05-25 13:21:56 -0800

    feat: set up scaffolding for methods in the Game class

---
+++ src/services/Game.ts
@@ -0,0 +1,24 @@
+class Game {
+  constructor() {}
+
+  // Start the game
+  run() {}
+
+  // Show the menu
+  printMenu() {}
+
+  //
+  fish() {}
+
+  //
+  viewInventory() {}
+
+  //
+  sellFish() {}
+
+  //
+  visitShop() {}
+
+  //
+  changeLocation() {}
+}
